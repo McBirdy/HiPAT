902d443e91fc669129305789957d1c0ca35586e9
Martin Nordsletten <mnordsletten@gmail.com>
2014-03-28 11:57:18 +0100

    New check_offset version
    Smarter method to get stable trusted average offset. Needs to implement
    the config reader still.

--- check_offset.py
+++ check_offset.py
@@ -5,16 +5,14 @@ Performs a check of the offset to the reference NTP server. Evaluates over time 
 returns: offset in ms to the reference server.
 """
 
-from config import config
+#from config import config
 import subprocess
 import time
 import sys
 import shelve
 import re
 import datetime
-import logger
-
-logfile = logger.init_logger("check_offset")
+import math
 
 def get_offset(raw_output = False):
     """Returns the offset between the client and the reference server. It first performs a check to see if ntpd is running.
@@ -22,7 +20,8 @@ def get_offset(raw_output = False):
     raw_output: return the complete ntpq -pn output
     returns: offset in ms to the reference server.
     """
-    ref_server = config["hipat_reference"]  #ntp reference server
+    #ref_server = config["hipat_reference"]  #ntp reference server
+    ref_server = "17.72.148.53"
     
     #if Ntpd isn't running we set the date manually and restart the service.
     ntpd_status = subprocess.call(["pgrep", "ntpd"], stdout=subprocess.PIPE)
@@ -40,63 +39,72 @@ def get_offset(raw_output = False):
     offset_ref = re.search(regex, ntpq_output, re.MULTILINE)
     offset_ref = float(offset_ref.group(1))
     return offset_ref
-   
+
+def calculate_average_std(offset_list):
+    """Will calculate an average and standard deviation based on the offset provided. 
     
-def avg1(average, offset):
-    """Calculates a new average based on the previous average and the new offset.
-    returns: average
+    offset_list: list of offsets to be calculated upon.
+    
+    returns:
+    average: float with calculated average
+    std: float with calculated standard deviation
     """
-    new_average = (float(average) * (2.0/3)) + (float(offset) * (1.0/3))  
+    average = sum(offset_list) / float(len(offset_list))
+    
+    variance = map(lambda x: (x - average)**2, offset_list)
+    average_variance = sum(variance) / float(len(variance))
+    standard_deviation = math.sqrt(average_variance)
+
+    return average, standard_deviation
+    
+def get_quality_offset():
+    """Will get the offset multiple times until it is sure of a range in the offset.
+    
+    returns: offset in float
+    """
+    #Local variables used in this function
+    offset_list = []            # List of the offsets, this list will always be 10 entries long.
+    confident_result = False    # When the average is trusted this is used to exit while loop.
+    std_limit = 1.0             # Standard deviation limit, this will increase for every loop.
+    
+    #Perform 9 get offsets to get an initial data set
+    for x in range(10):
+        offset_list.append(get_offset())
+        print offset_list
+        #offset_list = [10.2, 10.4, 11.1, 9.0, 12.1, 11.1, 10.0, 13.1, 11.0]
+        time.sleep(20)  #Sleep for 20 seconds. NTP update time is 16 seconds
+    
+    #Additional offsets are attained every loop and the standard deviation is evaluated.
+    while(confident_result == False):
+        
+        #Calculate average and std of old dataset
+        old_average, old_std = calculate_average_std(offset_list)
+        print "Prev avg: %s\nPrev std: %s" % (str(old_average), str(old_std))
+        print "Std limit: %s" % str(std_limit)
+        
+        #Get another offset, update offset_list and calculate average and std
+        offset_list.append(get_offset())
+        #x = raw_input("> ")
+        #offset_list.append(float(x))
+        offset_list = offset_list[1:]   #Remove first entry in list
+        new_average, new_std = calculate_average_std(offset_list)   #Calculate new average and standard deviation
+        print offset_list
+        print "New avg: %s\nNew std: %s" % (str(new_average), str(new_std))
+        
+        if new_std <= old_std and new_std <= std_limit:   #If the standard deviation is improving and is under the limit.
+            confident_result = True
+        else:   #if the new_std is larger than old_std (i.e. not improving) and is below the std_limit
+            time.sleep(20)
+        std_limit += 0.05    #Increase the limit for every loop
+        
     return new_average
 
-def avg2(compare_interval, counter_steps, offset, db):
-    """Avg2 is called when a new offset is outside the compare_interval of the trusted_average. Avg2 makes sure that the new offset is stable before it is trusted. It does this by making sure that the new offset is repeated counter_steps number of times.
+def main():
+    """Will return the offset to the reference server in ms. 
     
-    compare_interval = how far appart the offsets can be from the average to be accepted.
-    counter_steps = how many superseding values within the compare interval needed to become a trusted value.
-    offset: offset to the ntp reference
-    db: shelve file with trusted_average
-    
-    returns: nothing, but has updated the shelve file
+    return: offset in ms as float    
     """
-    untrusted_average = offset  #this is the starting point
-    for count in range(counter_steps):  #number of times we need a stable value
-        high_interval = untrusted_average + compare_interval
-        low_interval = untrusted_average - compare_interval
-        offset = get_offset()   #we get a new offset value
-        if low_interval <= offset <= high_interval:
-            untrusted_average = avg1(untrusted_average, offset)
-            time.sleep(60)
-        else:
-            return
-    db['average'] = untrusted_average #it is now trusted
-    return
-            
-    
-    
-def main(compare_interval, counter_steps):
-    """reads in program arguments, creates a shelve file. Then it checks wheter we do an avg1 or avg2 calculation. Finally the new average is written to the shelf and printed out.
-    
-    compare_interval = how far appart the offsets can be from the average to be accepted.
-    counter_steps = how many superseding values within the compare interval needed to become a trusted value.
-    
-    """    
-    offset = get_offset()
-    db = shelve.open(config['program_path']+'shelvefile', 'c')
-    trusted_average = db['average'] #the trusted average is stored in the shelf
-    
-    #check to see if we can perform average 1 calculation
-    high_interval = trusted_average + compare_interval
-    low_interval = trusted_average - compare_interval
-    if low_interval <= offset <= high_interval:
-        new_average = avg1(trusted_average, offset)
-        db['average'] = new_average #the new average is saved
-    else:
-        avg2(compare_interval, counter_steps, offset, db)
-    
-    new_average = db['average']
-    db.close()
-    return new_average   
+    print get_quality_offset()
 
 if __name__ == '__main__':
     main()
